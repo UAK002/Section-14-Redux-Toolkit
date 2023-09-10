e3614fd4262a3a4a723dab13107eb2dc148b67f3
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-09-11 00:48:59 +0530

    424. Cart Container

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Navbar from './Components/Navbar';
+import CartContainer from './Components/CartContainer';
 
 function App() {
   return (
     <main>
       <Navbar />
+      <CartContainer />
     </main>
   );
 }
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -2,9 +2,9 @@ import { createSlice } from '@reduxjs/toolkit';
 import cartItems from '../../cartItems';
 
 const initialState = {
-  //   cartItems: [],
   cartItems: cartItems,
-  amount: 0,
+  //   amount: 0,
+  amount: 4,
   total: 0,
   isLoading: true,
 };
