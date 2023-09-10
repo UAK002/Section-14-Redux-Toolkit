04f5981e4ff54f5bf17bc2a2ee804731ac0b5801
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-09-10 14:06:52 +0530

    419. First Slice

---
+++ src/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+// import './features/cart/cartSlice';
+import cartReducer from './features/cart/cartSlice';
+
+export const store = configureStore({
+  reducer: {
+    cart: cartReducer,
+  },
+});
